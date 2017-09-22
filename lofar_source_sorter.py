f447dc24057293c27d267be86ea94bab2385cb55
Wendy L. Williams <wndywllms@gmail.com>
2017-09-22 15:44:11 +0100

    testing overlapping/intersecting sources

--- lofar_source_sorter.py
+++ lofar_source_sorter.py
@@ -415,12 +415,120 @@ f_nn5_idx,f_nn5_sep2d,f_nn5_dist3d = ac.match_coordinates_sky(c,c,nthneighbor=5)
 #f_nn6_idx,f_nn6_sep2d,f_nn6_dist3d = ac.match_coordinates_sky(c,c,nthneighbor=6)
 
 
-lofarcat.add_column(Column(lofarcat['LR'][f_nn_idx], 'NN_LR'))
-lofarcat.add_column(Column(f_nn_sep2d.to(u.arcsec).value, 'NN_sep'))
-lofarcat.add_column(Column(f_nn5_sep2d.to(u.arcsec).value, 'NN5_sep'))
-lofarcat.add_column(Column(lofarcat['Total_flux'][f_nn_idx], 'NN_Total_flux'))
-lofarcat.add_column(Column(lofarcat['Maj'][f_nn_idx], 'NN_Maj'))
+if 'NN_sep' not in lofarcat.colnames:
+    lofarcat.add_column(Column(lofarcat['LR'][f_nn_idx], 'NN_LR'))
+    lofarcat.add_column(Column(f_nn_sep2d.to(u.arcsec).value, 'NN_sep'))
+    lofarcat.add_column(Column(f_nn_idx, 'NN_idx'))
+    lofarcat.add_column(Column(f_nn5_sep2d.to(u.arcsec).value, 'NN5_sep'))
+    lofarcat.add_column(Column(lofarcat['Total_flux'][f_nn_idx], 'NN_Total_flux'))
+    lofarcat.add_column(Column(lofarcat['Total_flux']/lofarcat['NN_Total_flux'], 'NN_Frat'))
+    lofarcat.add_column(Column(lofarcat['Maj'][f_nn_idx], 'NN_Maj'))
 
+
+
+#artefact = np.zeros(len(lofarcat),dtype=bool)
+#selind1 = np.where((lofarcat['Maj'] < 8.) & (lofarcat['Total_flux'] > 100.) & (lofarcat['NN5_sep'] < 60.))[0]
+#artefact[selind] =1
+
+
+
+####
+#### TESTING ### 
+import numpy as np
+from shapely.geometry.polygon import LinearRing
+from shapely.geometry import Polygon
+
+def ellipse_polyline(ellipses, n=100):
+    t = np.linspace(0, 2*np.pi, n, endpoint=True)
+    st = np.sin(t)
+    ct = np.cos(t)
+    result = []
+    for x0, y0, a, b, angle in ellipses:
+        angle = np.deg2rad(angle)
+        sa = np.sin(angle)
+        ca = np.cos(angle)
+        p = np.empty((n, 2))
+        p[:, 0] = x0 + a * ca * ct - b * sa * st
+        p[:, 1] = y0 + a * sa * ct + b * ca * st
+        result.append(p)
+    return result
+
+def ellipses_intersect(ellipse1, ellipse2):
+    a, b = ellipse_polyline((ellipse1, ellipse2))
+    ea = LinearRing(a)
+    eb = LinearRing(b)
+    mp = ea.intersection(eb)
+    #encloses = ea.contains(eb)
+    #encloses = ea.contains(Point(ellipse2[0], ellipse2[1]))
+    pa = Polygon(a)
+    pb = Polygon(b)
+    encloses = pa.contains(pb)
+
+    x = [p.x for p in mp]
+    y = [p.y for p in mp]
+    if len(x) > 0:
+        intersects = True
+    else:
+        intersects =  False
+    
+    return intersects, encloses
+
+
+calculate_intersections = False
+if calculate_intersections:
+    c = ac.SkyCoord(lofarcat['RA'], lofarcat['DEC'], unit="deg")
+    cintersects = np.zeros(len(lofarcat), dtype=bool)
+    cencloses = np.zeros(len(lofarcat), dtype=bool)
+    cenclosed = np.zeros(len(lofarcat), dtype=bool)
+    for i in range(len(lofarcat)):
+        #print i
+        l = lofarcat[i]
+        if l['NN_sep'] > l['Maj']:  continue
+        
+        ci = ac.SkyCoord([l['RA']], [l['DEC']], unit='deg')
+        idx, idxself, sep, _ = ci.search_around_sky(c, 2*l['Maj']*u.arcsec)
+        if len(idx) == 1:   
+            continue
+        
+        ii = idx
+        
+        for j in ii:
+            if j == i: continue
+            l1 = lofarcat[j]
+            
+            intersects, encloses = ellipses_intersect( (l['RA'], l['DEC'], l['Maj']/2., l['Min']/2, l['PA']), (l1['RA'], l1['DEC'], l1['Maj']/2., l1['Min']/2, l1['PA']) )
+            
+            print i, j, intersects, encloses, (l['RA'], l['DEC'], l['Maj']/2., l['Min']/2, l['PA']), (l1['RA'], l1['DEC'], l1['Maj']/2., l1['Min']/2, l1['PA'])
+            
+            if intersects:
+                cintersects[i] = True
+                cintersects[j] = True
+            if encloses:
+                cencloses[i] = True
+                cenclosed[j] = True
+    if 'Encloses' not in lofarcat.colnames:
+        lofarcat.add_column(Column(cencloses, 'Encloses'))
+    if 'Enclosed' not in lofarcat.colnames:
+        lofarcat.add_column(Column(cenclosed, 'Enclosed'))
+    if 'Intersects' not in lofarcat.colnames:
+        lofarcat.add_column(Column(cintersects, 'Intersects'))
+        
+
+    M_encloses = Mask(lofarcat['Encloses'],
+                    'Encloses',
+                    'encloses',
+                    qlabel='encloses?')
+    M_enclosed = Mask(lofarcat['Enclosed'],
+                    'Enclosed',
+                    'enclosed',
+                    qlabel='enclosed?')
+    M_intersects = Mask(lofarcat['Intersects'],
+                    'Intersects',
+                    'intersects',
+                    qlabel='intersects?')
+    M_encloses.make_sample(lofarcat)
+    M_enclosed.make_sample(lofarcat)
+    M_intersects.make_sample(lofarcat)
 
 ########################################################
 
