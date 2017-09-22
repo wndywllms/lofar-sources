1fe8bf881d661fcd09f6aeefc5895c97b504d245
Wendy L. Williams <wndywllms@gmail.com>
2017-09-22 16:03:26 +0100

    add arg for make_summary and document

--- make_summary.py
+++ make_summary.py
@@ -1,13 +1,17 @@
 import glob
 import os
+import sys
 
-ddir = 'test_gaus/'
+# this should be the directory where the sample fits files are
+ddir = sys.argv[1]
 
-fitslist = glob.glob(ddir+'*.fits')
+#look for all the sample fits files
+fitslist = glob.glob(ddir+'/*.fits')
 
 for f in fitslist:
     fdir = f.replace('.fits','')
-    filelist = glob.glob(fdir+'/*')
+    # get all the images for this sample
+    filelist = glob.glob(fdir+'/*.png')
     print filelist
     cmd = 'montage '+' '.join(filelist)+'  -tile 5x -geometry 256x256+1+1 '+fdir+'.pdf'
     os.system(cmd)
